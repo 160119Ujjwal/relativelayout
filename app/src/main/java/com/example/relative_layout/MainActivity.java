be2929d17ae2d11b7bc805c61ea14d8b5ae32cf5
ujjwal <ujjwalkhaitu@gmail.com>
2019-10-20 21:14:52 +0545

    final code

--- app/src/main/java/com/example/relative_layout/MainActivity.java
+++ app/src/main/java/com/example/relative_layout/MainActivity.java
@@ -10,20 +10,20 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button click;
-    ImageView image1, image2, image3, image4;
-    int n=0;
+    ImageView img1, img2, img3, img4;
+    int counter=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image1 = findViewById(R.id.imageView2);
-        image2 = findViewById(R.id.imageView3);
-        image3 = findViewById(R.id.imageView4);
-        image4 = findViewById(R.id.imageView5);
+        img1 = findViewById(R.id.iv1);
+        img2 = findViewById(R.id.iv2);
+        img3 = findViewById(R.id.iv3);
+        img4 = findViewById(R.id.iv4);
         click = findViewById(R.id.Click);
-
 
         click.setOnClickListener(this);
 
@@ -32,25 +32,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
-
-        n++;
-
-        if (n==1) {
-            image1.setImageResource(R.drawable.ic_launcher_foreground);
-
+        if(counter > 3){
+            counter = 0;
         }
 
-        else {
-              image2.setImageResource(R.drawable.ic_launcher_foreground);
-
-            }
-
+        if(counter == 0)
+        {
+            img1.setImageResource(R.drawable.ic_launcher_foreground);
+            img2.setImageResource(R.drawable.ic_launcher_background);
+            img3.setImageResource(R.drawable.ic_launcher_background);
+            img4.setImageResource(R.drawable.ic_launcher_background);
         }
 
+        if (counter == 1)
+        {
+            img1.setImageResource(R.drawable.ic_launcher_background);
+            img2.setImageResource(R.drawable.ic_launcher_foreground);
+            img3.setImageResource(R.drawable.ic_launcher_background);
+            img4.setImageResource(R.drawable.ic_launcher_background);
+        }
 
+        if (counter == 2)
+        {
+            img1.setImageResource(R.drawable.ic_launcher_background);
+            img2.setImageResource(R.drawable.ic_launcher_background);
+            img3.setImageResource(R.drawable.ic_launcher_foreground);
+            img4.setImageResource(R.drawable.ic_launcher_background);
+        }
+
+        if(counter == 3)
+        {
+            img1.setImageResource(R.drawable.ic_launcher_background);
+            img2.setImageResource(R.drawable.ic_launcher_background);
+            img3.setImageResource(R.drawable.ic_launcher_background);
+            img4.setImageResource(R.drawable.ic_launcher_foreground);
+        }
+        counter++;
     }
 }
+
 
 
 
